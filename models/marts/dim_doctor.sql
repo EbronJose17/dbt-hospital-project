31f74ad814e15ee42029781f9341957cc4f9fa81
Ebron Jose <ebronjose1724@gmail.com>
2025-07-16 07:21:52 +0000

    tag push

--- models/marts/dim_doctor.sql
+++ models/marts/dim_doctor.sql
@@ -3,6 +3,7 @@
         materialized = 'incremental',
         unique_key = 'doctor_id',
         schema = 'marts_schema',
+        merge_update_columns = [],
         pre_hook = "{{ log_model_start(this.name, invocation_id, model.config.materialized, target.database, model.config.schema) }}",
         post_hook = '{{ log_macro_end(this.name, invocation_id) }}',
         tags = ['dim', 'doctor']
