9d2e85b92a406c2db66bc01fe19bf6502d7e74d8
Ebron Jose <ebronjose1724@gmail.com>
2025-07-21 04:54:27 +0000

    implemented updated at record in dim doctor and treatment type

--- models/marts/dim_doctor.sql
+++ models/marts/dim_doctor.sql
@@ -3,23 +3,68 @@
         materialized = 'incremental',
         unique_key = 'doctor_id',
         schema = 'marts_schema',
-        merge_update_columns = ['specialization', 'phone_number', 'years_experience', 'hospital_branch', 'email'],
-        pre_hook = "{{ log_model_start(this.name, invocation_id, model.config.materialized, target.database, model.config.schema) }}",
-        post_hook = '{{ log_macro_end(this.name, invocation_id) }}',
-        tags = ['dim', 'doctor']
     )
 }}
 
-select 
-    doctor_sk,
-    doctor_id,
-    first_name,
-    last_name,
-    specialization,
-    phone_number,
-    years_experience,
-    hospital_branch,
-    email,
-    current_timestamp as updated_at
-from 
-    {{ref('int_doctors')}}
+with source as (
+    select 
+        doctor_sk,
+        doctor_id,
+        first_name,
+        last_name,
+        specialization,
+        phone_number,
+        years_experience,
+        hospital_branch,
+        email,
+        _dbt_updated_at as updated_at
+    from 
+        {{ref('int_doctors')}}
+)
+{% if is_incremental() %}
+
+
+,
+
+target as (
+    select 
+        doctor_sk,
+        doctor_id,
+        first_name,
+        last_name,
+        specialization,
+        phone_number,
+        years_experience,
+        hospital_branch,
+        email,
+        updated_at
+    from 
+        {{this}}
+),
+
+diff_data as (
+    select 
+        src.*
+    from 
+        source src left join target tgt 
+    on 
+        src.doctor_id = tgt.doctor_id
+    where 
+        tgt.doctor_id is null or
+        src.doctor_sk <> tgt.doctor_sk or
+        src.first_name <> tgt.first_name or 
+        src.last_name <> tgt.last_name or
+        src.specialization <> tgt.specialization or
+        src.phone_number <> tgt.phone_number or 
+        src.years_experience <> tgt.years_experience or 
+        src.hospital_branch <> tgt.hospital_branch or
+        src.email <> tgt.email
+)
+
+select * from diff_data
+
+{% else %}
+
+select * from source
+
+{% endif %}
--- models/marts/dim_treatment_type.sql
+++ models/marts/dim_treatment_type.sql
@@ -3,7 +3,6 @@
         materialized = 'incremental',
         unique_key = 'treatment_type_sk',
         schema = 'marts_schema',
-        merge_update_columns = ['treatment_type', 'treatment_description', 'treatment_cost_range'],
         pre_hook = "{{ log_model_start(this.name, invocation_id, model.config.materialized, target.database, model.config.schema) }}",
         post_hook = '{{ log_macro_end(this.name, invocation_id) }}',
         tags = ['dim', 'treatment']
@@ -29,12 +28,51 @@ final as (
     where rn = 1
 )
 
+{% if is_incremental() %}
+
+, existing as (
+    select 
+        treatment_type_sk,
+        treatment_type,
+        treatment_description,
+        treatment_cost_range,
+        created_at 
+    from 
+        {{this}}
+),
+
+changed_records as (
+    select 
+        f.treatment_type_sk,
+        f.treatment_type,
+        f.treatment_description,
+        f.treatment_cost_range,
+        e.created_at,
+        current_timestamp() as _dbt_updated_at
+    from 
+        final f left join existing e 
+    on 
+        f.treatment_type_sk = e.treatment_type_sk
+    where 
+        e.treatment_type_sk is null or 
+        e.treatment_type <> f.treatment_type or
+        e.treatment_description <> f.treatment_description or 
+        e.treatment_cost_range <> f.treatment_cost_range
+)
+
+select 
+    *
+from 
+    changed_records
+
+{% else %}
+
 select 
     *,
-    current_timestamp() as _dbt_updated_at 
+    current_timestamp() as created_at,
+    current_timestamp() as _dbt_updated_at
 from 
-    final
+    final 
 
--- {% if is_incremental() %}
---     where updated_at > (select max(updated_at) from {{ this }}) 
--- {% endif %}
+{% endif %}
+
